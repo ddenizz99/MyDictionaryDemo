6f293646f76046f4bf5e6e1ce3c3ad1fe65da514
Ege Deniz <ege_denzo_99@hotmail.com>
2021-01-23 18:31:04 +0300

    Add project files.

---
+++ MyDictionaryDemo/MyDictionary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryDemo
+{
+    class MyDictionary<K,V>
+    {
+        public K[] Key { get; set; }
+
+        public V[] Value { get; set; }
+
+        public int Count
+        {
+            get { return Key.Length; }
+        }
+
+        public MyDictionary()
+        {
+            Key   = new K[0];
+            Value = new V[0];
+        }
+
+        public void Add(K key, V val)
+        {
+            K[] Tkey = Key;
+            V[] Tval = Value;
+
+            Key   = new K[Key.Length + 1];
+            Value = new V[Value.Length + 1];
+            for (int i = 0; i < Tkey.Length; i++)
+            {
+                Key[i]   = Tkey[i];
+                Value[i] = Tval[i];
+            }
+
+            Key[Key.Length - 1]     = key;
+            Value[Value.Length - 1] = val;
+        }
+    }
+}
---
+++ MyDictionaryDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> menuUrl = new MyDictionary<string, string>();
+
+            menuUrl.Add("Home", "index.php");
+            menuUrl.Add("About US", "about-us.php");
+            menuUrl.Add("Menu", "menu.php");
+            menuUrl.Add("Contact", "contact.php");
+
+            for (int i = 0; i < menuUrl.Count; i++)
+            {
+                Console.WriteLine("<li><a href=\"{0}\">{1}</a></li>", menuUrl.Value[i], menuUrl.Key[i]);
+            }
+
+            Console.WriteLine("Menu Item ({0})", menuUrl.Count);
+
+            Console.ReadKey();
+        }
+    }
+}
